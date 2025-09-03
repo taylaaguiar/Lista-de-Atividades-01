36d8440dac6df7b3b0da06f67abb9461162bab4c
Tayla <tayla2013770109dem@gmail.com>
2025-09-03 11:27:49 -0300

    Feat: Atividade 02

---
+++ Atividade 02/Atividade 02/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num;
+Console.WriteLine("Escreva um número para saber seu antecedente!");
+num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"O antecessor é: {num - 1}");
