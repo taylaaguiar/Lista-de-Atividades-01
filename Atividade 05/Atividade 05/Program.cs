5b576d71cf83502a45bfce61164d2bd6732803a8
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 08:52:09 -0300

    Feat: Atividade 05

---
+++ Atividade 05/Atividade 05/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+        Console.Write("Digite o número total de eleitores: ");
+        int totalEleitores = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de votos brancos: ");
+        int votosBrancos = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de votos nulos: ");
+        int votosNulos = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de votos válidos: ");
+        int votosValidos = int.Parse(Console.ReadLine());
+
+        double percBrancos = (double)votosBrancos / totalEleitores * 100;
+        double percNulos = (double)votosNulos / totalEleitores * 100;
+        double percValidos = (double)votosValidos / totalEleitores * 100;
+
+        Console.WriteLine($"\nPercentual de votos brancos: {percBrancos:F2}%");
+        Console.WriteLine($"Percentual de votos nulos: {percNulos:F2}%");
+        Console.WriteLine($"Percentual de votos válidos: {percValidos:F2}%");
+
+
+
