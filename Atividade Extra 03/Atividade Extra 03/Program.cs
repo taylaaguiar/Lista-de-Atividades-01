f3f79777950857e75f6cbb11a0711aa7e0f8b997
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 11:18:55 -0300

    Feat: Atividade Extra 03

---
+++ Atividade Extra 03/Atividade Extra 03/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite o valor de A: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor de B: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor de C: ");
+double c = double.Parse(Console.ReadLine());
+
+double delta = Math.Pow(b, 2) - 4 * a * c;
+
+Console.WriteLine($"\nO valor de Delta é: {delta:F2}");
