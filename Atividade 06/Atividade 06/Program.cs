6f82727ca04bad5b5287020899a66d3cd1a155e7
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 09:05:27 -0300

    Feat: Atividade 06

---
+++ Atividade 06/Atividade 06/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite o salário atual do funcionário: R$ ");
+double salarioAtual = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o percentual de reajuste (%): ");
+double percentualReajuste = double.Parse(Console.ReadLine());
+
+double novoSalario = salarioAtual + (salarioAtual * percentualReajuste / 100);
+
+Console.WriteLine($"\nO novo salário do funcionário é: R$ {novoSalario:F2}");
