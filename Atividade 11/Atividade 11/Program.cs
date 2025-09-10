ed9db2859f17af08741c05477cec731bb6d1e611
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 09:41:58 -0300

    Feat: Atividade 11

---
+++ Atividade 11/Atividade 11/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite o ano atual: ");
+int anoAtual = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o ano de nascimento: ");
+int anoNascimento = int.Parse(Console.ReadLine());
+
+int idade = anoAtual - anoNascimento;
+
+Console.WriteLine($"\nIdade calculada: {idade} anos");
+
+if (idade < 16)
+{
+    Console.WriteLine("Você NÃO pode votar este ano.");
+}
+else if ((idade >= 16 && idade < 18) || idade >= 70)
+{
+    Console.WriteLine("Seu voto é FACULTATIVO.");
+}
+else
+{
+    Console.WriteLine("Você é OBRIGADO a votar este ano.");
+}
