206243bec0b95102cfbb4cbc73e173c677352f43
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 09:19:52 -0300

    Feat: Atividade 09

---
+++ Atividae 09/Atividae 09/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite o número de maçãs compradas: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+double precoUnitario = quantidade < 12 ? 1.30 : 1.00;
+double total = quantidade * precoUnitario;
+
+Console.WriteLine($"\nO custo total da compra é: R$ {total:F2}");
