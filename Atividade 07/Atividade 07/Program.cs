b35eb1891cede1f62cc792c4c79cb012549159b8
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 09:10:08 -0300

    Feat: Atividade 07

---
+++ Atividade 07/Atividade 07/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite um número: ");
+int valor = int.Parse(Console.ReadLine());
+
+if (valor > 10)
+{
+    Console.WriteLine("É MAIOR QUE 10!");
+}
+else
+{
+    Console.WriteLine("NÃO É MAIOR QUE 10");
+}
