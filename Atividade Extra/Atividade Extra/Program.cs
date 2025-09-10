e73f131008971adfce43b6059ad6257a68fd4e9f
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 11:03:29 -0300

    Feat: Atividade Extra

---
+++ Atividade Extra/Atividade Extra/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite quanto dinheiro você tem na carteira (em R$): ");
+double reais = double.Parse(Console.ReadLine());
+
+double taxaDolar = 3.45;
+double dolares = reais / taxaDolar;
+
+Console.WriteLine($"\nCom R$ {reais:F2}, você pode comprar aproximadamente US$ {dolares:F2}");
+
