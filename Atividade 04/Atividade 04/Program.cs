fb139c163ed456c2ceefe3a549d14663bf2d0e13
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 08:28:51 -0300

    Feat: Atividade 04

---
+++ Atividade 04/Atividade 04/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int ida;
+Console.WriteLine("escreva a sua idade aqui: ");
+ida = int.Parse(Console.ReadLine());
+Console.WriteLine($"a sua idade calculado em dias é gual a: {ida * 360} dias");
