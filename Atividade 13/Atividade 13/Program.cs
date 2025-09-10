406f8621dd3530026c4159fda770986af7149373
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 10:19:03 -0300

    Feat: Atividade 13

---
+++ Atividade 13/Atividade 13/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> numeros = new List<int>();
+
+Console.Write("Digite o primeiro número: ");
+numeros.Add(int.Parse(Console.ReadLine()));
+
+Console.Write("Digite o segundo número: ");
+numeros.Add(int.Parse(Console.ReadLine()));
+
+Console.Write("Digite o terceiro número: ");
+numeros.Add(int.Parse(Console.ReadLine()));
+
+numeros.Sort(); // Ordena em ordem crescente
+
+Console.WriteLine("\nOs números em ordem crescente são:");
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
