168c8153b2ed5c1085efe0168bb33449fee5c524
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 09:49:57 -0300

    Feat: Atividade 12

---
+++ Atividade 12/Atividade 12/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double n1;
+double n2;
+Console.WriteLine("DIGITE SEU PRIMEIRO VALOR: ");
+n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("DIGITE SEU SEGUNDO VALOR: ");
+n2 = double.Parse(Console.ReadLine());
+if (n1 < n2)
+{
+    Console.WriteLine($"{n1} É MENOR QUE {n2}");
+}
+else
+{
+    Console.WriteLine($"{n2} É MENOR QUE {n1}");
+}
