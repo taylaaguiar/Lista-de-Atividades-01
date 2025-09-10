a53958ad002dff2668e599d7a1b54ebf9209d439
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 08:26:08 -0300

    Feat: Aitividade 03

---
+++ Atividde 03/Atividde 03/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int bas;
+int alt;
+Console.WriteLine("digite o seu valor de base: ");
+bas = int.Parse(Console.ReadLine());
+Console.WriteLine("digite o seu valor de altura: ");
+alt = int.Parse(Console.ReadLine());
+Console.WriteLine($"o valor da sua área é: {alt * bas}");
+
+
+
+
+
+
+
+
+
+
