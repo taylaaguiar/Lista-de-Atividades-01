29258f86b827bc379bf98e0871c3b78fb7687b85
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 09:13:36 -0300

    Feat: Atividade 08

---
+++ Atividade 08/Atividade 08/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    Console.Write("Digite um número (diferente de zero): ");
+    int valor = int.Parse(Console.ReadLine());
+
+    if (valor == 0)
+    {
+        Console.WriteLine("Valor inválido! O número não pode ser zero.");
+    }
+    else if (valor > 0)
+    {
+        Console.WriteLine("O número é POSITIVO.");
+    }
+    else
+    {
+        Console.WriteLine("O número é NEGATIVO.");
+    }
