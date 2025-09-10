b352017551f7a9ffd634e928fc26b051be31fc28
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-10 11:14:40 -0300

    Feat: Atividade Extra 02

---
+++ Atividade Extra 02/Atividade Extra 02/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+        Console.Write("Digite a largura da parede (em metros): ");
+        double largura = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura da parede (em metros): ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double area = largura * altura;
+        double litrosTinta = area / 2;
+
+        Console.WriteLine($"\nÁrea da parede: {area:F2} m²");
+        Console.WriteLine($"Quantidade de tinta necessária: {litrosTinta:F2} litros");
