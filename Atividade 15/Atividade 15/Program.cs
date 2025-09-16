a6680f0d5e91f8c5b8dffb6db6cda182d769c753
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-16 08:57:07 -0300

    Feat: Atividade 15

---
+++ Atividade 15/Atividade 15/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    Console.WriteLine("Números entre 1000 e 2000 com resto 5 ao dividir por 11:");
+    Console.WriteLine("--------------------------------------------------------");
+
+    for (int i = 1000; i <= 2000; i++)
+    {
+        if (i % 11 == 5)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    Console.WriteLine("--------------------------------------------------------");
+    Console.WriteLine("Fim da verificação.");
+    Console.ReadKey();
+}
