6faa984352fc810ede19bce18e65f13c84e68329
Tayla Aguiar <tayla2013770109dem@gmail.com>
2025-09-16 08:51:31 -0300

    Feat: Atividade 14

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num;
+Console.WriteLine("digite um número aqui: ");
+num = int.Parse(Console.ReadLine());
+
+for (int contador = 0; contador <= num; contador++)
+{
+    Console.WriteLine(contador);
+
+}
